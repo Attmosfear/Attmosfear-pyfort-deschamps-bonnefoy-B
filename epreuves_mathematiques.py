99145a2dad18d255b2dabdd0dc31b5e94701af64
Attmosfears <deschmalo@icloud.com>
2024-12-06 10:53:18 +0100

    Epreuve equation lineaire faite

--- epreuves_mathematiques.py
+++ epreuves_mathematiques.py
@@ -1,10 +1,18 @@
-yfgzevkuyfjhbef
-zeihfvsekukfb
-zejfegskuj;bzs
-rihesgfkfes
-qzefuysebf
-zeifbsks
-edniuksdgx
-endiugbsekuid
-esrnkjsebd
-er,niuhsegvdf
+from random import *
+def resoudre_equation_lineaire():
+    a = randint(1,10)
+    b = randint(1,10)
+    x = -b/a
+    return  a, b, x
+
+def epreuve_math_equation():
+    res, a, b = resoudre_equation_lineaire()
+    print(" Résoudre l'équation : ", a,"x + ", b, " = 0" )
+    val = float(input("Quelle est la valeur de x:"))
+    if val == res:
+        return True
+    else:
+        return False
+
+
+

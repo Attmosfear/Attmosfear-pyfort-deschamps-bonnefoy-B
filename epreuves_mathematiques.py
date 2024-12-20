d9d561123e8c8642bda24a70a6b9c63ae2a01a5b
Attmosfears <deschmalo@icloud.com>
2024-12-20 11:22:17 +0100

    Epreuve maths roulette fin

--- epreuves_mathematiques.py
+++ epreuves_mathematiques.py
@@ -1,4 +1,5 @@
-from random import*
+from operator import is_not
+from random import *
 import random
 from math import*
 
@@ -30,7 +31,7 @@ def resoudre_equation_lineaire():
 
 def epreuve_math_equation():
     res, a, b = resoudre_equation_lineaire()
-    print(" Résoudre l'équation : ", a,"x + ", b, " = 0" )
+    print("Résoudre l'équation : ", a,"x + ", b, "= 0" )
     val = float(input("Quelle est la valeur de x:"))
     if val == res:
         return True
@@ -38,18 +39,35 @@ def epreuve_math_equation():
         return False
 
 def epreuve_roulette_mathematique():
-    tableau_nombre = [randint(1, 20)]*5
-    opp = random('+', '-', 'x')
+    tableau_nombre = []
+    res = 0
+    for i in range(5):
+        tableau_nombre.append(randint(1, 20))
+    print("Nombre de la roulette : ", tableau_nombre)
+    tableau_opp = ['+', '-', 'x']
+    index = randint(0, len(tableau_opp)-1)
+    opp = tableau_opp[index]
     if opp == '+':
         for i in range(len(tableau_nombre)):
+            res = 0
             res =+ tableau_nombre[i]
+        print("Calculez le résultat en combinant ces nombres avec une addition")
     elif opp == '-':
         for i in range(len(tableau_nombre)):
+            res = 0
             res =- tableau_nombre[i]
+        print("Calculez le résultat en combinant ces nombres avec une soustraction")
     else:
         for i in range(len(tableau_nombre)):
             res = 1
             res = res * tableau_nombre[i]
+        print("Calculez le résultat en combinant ces nombres avec une multiplication")
+
+    proposition = int(input("Votre réponse : "))
+    if  proposition != res:
+        return False
+    return True
+
 
 def est_premier(n):
     if n <= 1:
@@ -63,3 +81,5 @@ def premier_plus_proche(n):
     while not est_premier(n):
         n = n + 1
     return n
+
+print(epreuve_roulette_mathematique())
